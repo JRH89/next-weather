f6e737b6195245f313fecb4c4fa4ea60fbc7ad4d
JRH89 <thegemlins@gmail.com>
2023-06-06 22:19:59 -0700

    ok

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta property="og:url" content="https://weather-pal.vercel.app/" />
+        <meta property="og:title" content="Weather Pal" />
+        <meta property="og:description" content="Weather app made with React, Next, and Tailwind." />
+        <meta property="og:image" content="https://jareds-portfolio.vercel.app/images/preview.png" />
+        <meta property="url" content="https://weather-pal.vercel.app" />
+        <meta property="title" content="Weather Pal" />
+        <meta property="description" content="Weather app made with React, Next, and Tailwind." />
+        <meta property="image" content="https://jareds-portfolio.vercel.app/images/preview.png" />
+        <link type="image/png" rel="shortcut icon" href="hookerhill.png" />
+
+      </Head>
       <body>
         <Main />
         <NextScript />
