6c9e6cdca4a94a8e1f6a82180e7bcad7865ac718
Jared Hooker <68973848+JRH89@users.noreply.github.com>
2023-10-12 22:20:54 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
 	return (
 		<footer className="flex justify-center fixed p-2 self-center w-full bg-whiter bottom-0">
-			<a className="hover:opacity-60 hover:scale-90" href="https://hooker-hill-studios.vercel.app">
+			<a className="hover:opacity-60 hover:scale-90" href="https://www.hookerhillstudios.com">
 				Hooker Hill Studios © 2023
 			</a>
 		</footer>
